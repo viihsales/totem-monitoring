1633fd8c58b1b238e2415bf66fae79d6fcdd230f
Aluno <Aluno@Digital.net>
2019-09-17 18:23:52 -0300

    Iniciando cadastrar

--- site/public/js/functions_banco.js
+++ site/public/js/functions_banco.js
@@ -17,11 +17,40 @@ function logar() {
     if (response.ok) {
       response.json().then(function (resposta) {
         sessionStorage.usuario = resposta.email_admin;
+        sessionStorage.id_usuario = resposta.id; // rever o nome deste campo na tabela
         verificarAutenticacao();
       });
     } else {
       console.log('Erro de login!');
       alert('Login ou senha inválido.')
+      end_wait();
+    }
+  });
+
+  return false;
+}
+
+function cadastrarFuncionario() {
+
+  wait();
+
+  var formulario = new URLSearchParams(new FormData(form_cadastro));
+
+  fetch('/usuarios/cadastro_funcionario', {
+    method: "POST",
+    body: formulario
+  }).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (resposta) {
+        if (resposta == true){
+          window.location.href = 'index.html';
+        } else {
+          alert('Desculpe, Algo deu errado');
+          end_wait();
+        }
+      });
+    } else {
+      console.log('Erro de cadastro!');
       end_wait();
     }
   });
--- site/routes/usuarios.js
+++ site/routes/usuarios.js
@@ -36,4 +36,40 @@ router.post('/login', function (req, res, next) {
 
 });
 
+router.post('/cadastro_funcionario', function (req, res, next) {
+
+  banco.conectar().then(() => {
+    console.log(`Chegou p/ Cadastro: ${JSON.stringify(req.body)}`);
+
+    var nome = req.body.nome;
+    var email = req.body.email;
+    var cpf = req.body.cpf;
+    var senha = req.body.password;
+
+    if (nome == undefined || email == undefined || cpf == undefined || password == undefined) {
+      throw new Error(`Algo de errado não está certo: ${nome} / ${email} / ${cpf} / ${senha}`);
+    }
+    return banco.sql.query(`select * from user_admin where email_admin='${email}' and senha_admin='${senha}'`);
+  }).then(consulta => {
+
+    console.log(`Usuários encontrados: ${JSON.stringify(consulta.recordset)}`);
+
+    if (consulta.recordset.length == 1) {
+      res.send(consulta.recordset[0]);
+    } else {
+      res.sendStatus(404);
+    }
+
+  }).catch(err => {
+
+    var erro = `Erro no login: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
 module.exports = router;
